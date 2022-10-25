8440192a6394cba2a074a0b7774d0bc02ebaa5df
Bilen Tamrat <bilentamrat1@gmail.com>
2022-10-25 16:28:58 -0400

    Add elements to HTML through Javascript

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-console.log("We in HERE!")
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+console.log('We in HERE!');
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('GET', theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
-const url="https://periodic-table-elements-info.herokuapp.com/elements"
-const response = httpGet(url)
-console.log("We are here!",response)
+const url = 'https://periodic-table-elements-info.herokuapp.com/elements';
+const response = httpGet(url);
+console.log('We are here!', response);
+
+const paragraph = document.createElement('p');
+const content = document.createTextNode('We in here!');
+paragraph.appendChild(content);
+const div1 = document.getElementById('div1');
+div1.appendChild(paragraph);
+console.log('At the end of the file');
