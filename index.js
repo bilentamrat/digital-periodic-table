ff802192f1583735024b5912b768dc77236443bb
ty <tyranny.smith10@gmail.com>
2022-10-28 15:02:28 -0400

    Change API call to use FETCH

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 console.log('We in HERE!');
-function httpGet(theUrl) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('GET', theUrl, false); // false for synchronous request
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
-}
+async function loadElements() {
+    const URL = 'https://periodic-table-elements-info.herokuapp.com/elements';
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    console.log("The data", data)
+    //if (data.Response == "True") displayMovieList(data.Search);
+  }
+loadElements()
 const url = 'https://periodic-table-elements-info.herokuapp.com/elements';
-const response = httpGet(url);
-console.log('We are here!', response);
+//const response = httpGet(url);
+//console.log('We are here!', response);
 
 const paragraph = document.createElement('p');
 const content = document.createTextNode('We in here!');
@@ -16,13 +18,13 @@ const div1 = document.getElementById('div1');
 div1.appendChild(paragraph);
 console.log('At the end of the file');
 
-function addElements(theElements) {
-  theElements = theElements.json();
-  console.log('Before looping', theElements);
-  console.log('Get individual element', theElements[0]);
-  for (let i = 0; i < theElements.length; i++) {
-    const currentElement = theElements[i];
+//function addElements(theElements) {
+  //theElements = theElements.json();
+  //console.log('Before looping', theElements);
+  //console.log('Get individual element', theElements[0]);
+  //for (let i = 0; i < theElements.length; i++) {
+   // const currentElement = theElements[i];
     //console.log('The currentElement', currentElement);
-  }
-}
-addElements(response);
+ // }
+//}
+//addElements(response);
