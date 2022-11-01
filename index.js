8da115d90fc8d3dc804e846740feda263163416b
JLewis247 <donnell.lewis24@gmail.com>
2022-11-01 15:04:03 -0500

    delete unnecessary code and update add elements to display each element name

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ async function loadElements() {
     const URL = 'https://periodic-table-elements-info.herokuapp.com/elements';
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    //console.log("The data", data)
     return data
     //if (data.Response == "True") displayMovieList(data.Search);
   }
 const elements = loadElements()
-const url = 'https://periodic-table-elements-info.herokuapp.com/elements';
-//const response = httpGet(url);
-//console.log('We are here!', response);
 
-const paragraph = document.createElement('p');
-const content = document.createTextNode('We in here!');
-paragraph.appendChild(content);
-const div1 = document.getElementById('div1');
-div1.appendChild(paragraph);
+
 console.log('At the end of the file');
 
 async function addElements(theElements) {
@@ -25,6 +17,12 @@ async function addElements(theElements) {
   console.log('Get individual element', theElements[0]);
   for (let i = 0; i < theElements.length; i++) {
    const currentElement = theElements[i];
+   const paragraph = document.createElement('p');
+  const content = document.createTextNode(currentElement.name);
+  paragraph.appendChild(content);
+  const div1 = document.getElementById('div1');
+  div1.appendChild(paragraph);
+
     console.log('The currentElement', currentElement);
  }
 }
