b743eae1fa715afacd48c48437323a57fd4a72d2
Bilen Tamrat <bilentamrat1@gmail.com>
2022-11-10 15:05:26 -0500

    Fix bug

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 console.log('We in HERE!');
 async function loadElements() {
-    const URL = 'https://periodic-table-elements-info.herokuapp.com/elements';
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    return data
-    //if (data.Response == "True") displayMovieList(data.Search);
-  }
-const elements = loadElements()
-
-async function getElement (name){
-  const URL = `https://periodic-table-elements-info.herokuapp.com/element/atomicName/${name}`;
-  //var name = 
+  const URL = 'https://periodic-table-elements-info.herokuapp.com/elements';
   const res = await fetch(`${URL}`);
   const data = await res.json();
-return data 
+  return data;
+  //if (data.Response == "True") displayMovieList(data.Search);
 }
-const element = getElement('sodium')
-console.log('element', element);
+const elements = loadElements();
 
-console.log('At the end of the file');
-
-async function addElements(theElements) {
-  theElements = await theElements;
-  console.log('Before looping', theElements);
-  console.log('Get individual element', theElements[0]);
-  for (let i = 0; i < theElements.length; i++) {
-   const currentElement = theElements[i];
-   const paragraph = document.createElement('p');
-  const content = document.createTextNode(currentElement.name);
+async function getElement(name) {
+  const URL = `https://periodic-table-elements-info.herokuapp.com/element/atomicName/${name}`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+  return data;
+}
+async function retrieveElement(element) {
+  element = await getElement(element);
+  console.log('sodium', element);
+  const paragraph = document.createElement('p');
+  const content = document.createTextNode(element);
   paragraph.appendChild(content);
   const div1 = document.getElementById('div1');
   div1.appendChild(paragraph);
+}
+retrieveElement('sodium');
 
-    console.log('The currentElement', currentElement);
- }
+//console.log('At the end of the file');
+
+async function addElements(theElements) {
+  theElements = await theElements;
+  //console.log('Before looping', theElements);
+  //console.log('Get individual element', theElements[0]);
+  for (let i = 0; i < theElements.length; i++) {
+    const currentElement = theElements[i];
+    const paragraph = document.createElement('p');
+    const content = document.createTextNode(currentElement.name);
+    paragraph.appendChild(content);
+    const div1 = document.getElementById('div1');
+    div1.appendChild(paragraph);
+
+    //console.log('The currentElement', currentElement);
+  }
 }
 addElements(elements);
