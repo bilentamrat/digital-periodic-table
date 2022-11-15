1264263aec172ae150f107c2c92c4690a6d1f59b
JLewis247 <donnell.lewis24@gmail.com>
2022-11-15 15:06:02 -0600

    Button for modal added

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
 console.log('We in HERE!');
-async function loadElements() {
+async function loadAtoms() {
   const URL = 'https://periodic-table-elements-info.herokuapp.com/elements';
   const res = await fetch(`${URL}`);
   const data = await res.json();
   return data;
   //if (data.Response == "True") displayMovieList(data.Search);
 }
-const elements = loadElements();
+const atoms = loadAtoms();
 
-async function getElement(name) {
+async function getAtom(name) {
   const URL = `https://periodic-table-elements-info.herokuapp.com/element/atomicName/${name}`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
   return data;
 }
-async function retrieveElement(element) {
-  element = await getElement(element);
-  console.log('sodium', element);
+async function retrieveAtom(atom) {
+ atom = await getAtom(atom);
+  console.log('sodium', atom);
   const paragraph = document.createElement('p');
-  const content = document.createTextNode(element);
+  const content = document.createTextNode(atom);
   paragraph.appendChild(content);
   const div1 = document.getElementById('div1');
   div1.appendChild(paragraph);
 }
-retrieveElement('sodium');
+retrieveAtom('sodium');
 
 //console.log('At the end of the file');
 
-async function addElements(theElements) {
-  theElements = await theElements;
-  //console.log('Before looping', theElements);
-  //console.log('Get individual element', theElements[0]);
-  for (let i = 0; i < theElements.length; i++) {
-    const currentElement = theElements[i];
+async function addAtoms(theAtoms) {
+  theAtoms = await theAtoms;
+  //console.log('Before looping', theAtoms);
+  //console.log('Get individual element', theAtoms[0]);
+  for (let i = 0; i < theAtoms.length; i++) {
+    const currentAtom = theAtoms[i];
     const paragraph = document.createElement('p');
-    const content = document.createTextNode(currentElement.name);
+    const content = document.createTextNode(currentAtom.name);
     paragraph.appendChild(content);
     const div1 = document.getElementById('div1');
     div1.appendChild(paragraph);
 
-    //console.log('The currentElement', currentElement);
+    //console.log('The currentAtom', currentAtom);
   }
 }
-addElements(elements);
+addAtoms(atoms);
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+console.log('myModal', modal)
+console.log('document', document)
+// Get the button that opens the modal
+var btn = document.getElementById("btn");
+console.log('btn', btn)
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
