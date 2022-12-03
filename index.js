4e0fded32fe18f23ca03183f530cc768974b4d00
kheruhmiinah <caraminapereachamblee94@gmail.com>
2022-12-03 18:37:48 -0500

    new json file, new loop to make table

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const atomKeys = [
   'atomicNumber',
   'symbol',
@@ -23,32 +24,61 @@ const atomKeys = [
   'period',
   'group',
 ];
+*/
+const atomKeys = [
+  "name",
+  "appearance",
+  "atomic_mass",
+  "boil",
+  "category",
+  "density",
+  "discovered_by",
+  "melt",
+  "molar_heat",
+  "named_by",
+  "number",
+  "period",
+  "phase",
+  "xpos",
+  "ypos",
+  "symbol",
+  "electron_configuration",
+];
+
 //console.log('We in HERE!');
 async function loadAtoms() { 
-  const URL = "./data.json"//'https://secure-oasis-28926.herokuapp.com/https://periodic-table-elements-info.herokuapp.com/elements';
+  //const URL = "./data.json"//'https://secure-oasis-28926.herokuapp.com/https://periodic-table-elements-info.herokuapp.com/elements';
+  const URL = "./periodic.json";//'https://secure-oasis-28926.herokuapp.com/https://periodic-table-elements-info.herokuapp.com/elements';
   const res = await fetch(`${URL}`);
   const data = await res.json();
-  //var mydata = JSON.parse(data)
-  console.log("THE DATA", data);
-
   return data;
-  //if (data.Response == "True") displayMovieList(data.Search);
 }
 const atoms = loadAtoms();
 
 async function getAtom(name) {
-    const URL = `https://secure-oasis-28926.herokuapp.com/https://periodic-table-elements-info.herokuapp.com/element/atomicName/${name}`;
+  //const URL = `https://secure-oasis-28926.herokuapp.com/https://periodic-table-elements-info.herokuapp.com/element/atomicName/${name}`;
+  const URL = "./periodic.json";
   const res = await fetch(`${URL}`);
-  const data = await res.json();
+  const elements = await res.json();
+  var data;
+  for (let i = 0; i < 118; i++) {
+    const currentAtom = elements[i];
+    if(currentAtom.name == name){
+      data = currentAtom;
+      console.log(data);
+    }
+  }
   return data;
 }
+
 async function retrieveAtom(atom) {
   atom = await getAtom(atom);
   //console.log('sodium', atom);
   // next line clears the html in the "atom-content" paragraph (innerHTML)
   document.getElementsByClassName('atom-content')[0].innerHTML = "";
   for (let i = 0; i < atomKeys.length; i++) {
-    const atomValue = atom[0][`${atomKeys[i]}`];
+    //const atomValue = atom[0][`${atomKeys[i]}`];
+    const atomValue = atom[`${atomKeys[i]}`];
     if (atomValue != 'unknown') {
       console.log('Getting value', atomValue, 'From key', atomKeys[i]);
       const content = document.createTextNode(atomKeys[i]);
@@ -70,14 +100,16 @@ async function retrieveAtom(atom) {
 
 async function createTables(theAtoms) {
   theAtoms = await theAtoms;
-
+  console.log("THE ATOMS", theAtoms);
   const div1 = document.getElementById('div1');
   const table = document.createElement('table');
   table.setAttribute('id','periodic_table');
   table.setAttribute('align','center');
 
   for (let i = 1; i < 8; i++) {
-      let table_row = document.createElement('tr');          let table_cell = document.createElement('td');
+      let table_row = document.createElement('tr');
+      for (let j = 1; j < 19; j++) {
+          let table_cell = document.createElement('td');
           table_cell.setAttribute('id', "table_1_" + i + "_" + j);
           table_row.appendChild(table_cell);
       }
@@ -100,13 +132,15 @@ async function createTables(theAtoms) {
   }
   div1.appendChild(table2);
 
-  for (let i = 0; i < theAtoms.length; i++) {
+  //for (let i = 0; i < theAtoms.length; i++) {
+  for (let i = 0; i < 118; i++) {
     const currentAtom = theAtoms[i];
 
     const atomBox = document.createElement('div');
     atomBox.setAttribute('id', 'atom-box');
     const p1 = document.createElement('p');
-    const content = document.createTextNode(currentAtom.atomicNumber);
+    //const content = document.createTextNode(currentAtom.atomicNumber);
+    const content = document.createTextNode(currentAtom.number);
     p1.appendChild(content);
     const p2 = document.createElement('p');
     const content1 = document.createTextNode(currentAtom.symbol);
@@ -115,23 +149,28 @@ async function createTables(theAtoms) {
     const content2 = document.createTextNode(currentAtom.name);
     p3.appendChild(content2);
 
-    var row = currentAtom.period;
-    var col = currentAtom.group;
+    //var row = currentAtom.period;
+    //var col = currentAtom.group;
+    var row = currentAtom.ypos;
+    var col = currentAtom.xpos;
 
     atomBox.appendChild(p1);
     atomBox.appendChild(p2);
     atomBox.appendChild(p3);
 
-
-    const atomicNum = parseInt(currentAtom.atomicNumber);
+    //const atomicNum = parseInt(currentAtom.atomicNumber);
+    const atomicNum = parseInt(currentAtom.number);
     console.log(atomicNum);
-    //Boron is just wrong
+    console.log(col);
+    /*
+    Boron is just wrong
     if (atomicNum == 5){
       row = "2";
-    }
+    }*/
 
-    if(atomicNum < 57 || atomicNum > 102 || (atomicNum > 70 && atomicNum < 89)){
+    if(atomicNum < 57 || atomicNum > 103 || (atomicNum > 71 && atomicNum < 89)){
       periodicTableLocation = document.getElementById("table_1_" + row + '_' + col);
+      console.log("table_1_" + row + '_' + col);
       periodicTableLocation.appendChild(atomBox);
     }
     if(atomicNum >= 57 && atomicNum <= 70 )
@@ -142,6 +181,16 @@ async function createTables(theAtoms) {
       periodicTableLocation.appendChild(atomBox);
 
     }
+    // Atomic 71 is wrong
+    if(atomicNum == 71){
+      periodicTableLocation = document.getElementById("table_1_6_3");
+      periodicTableLocation.appendChild(atomBox);     
+    }
+    // Atomic 103 is wrong
+    if(atomicNum == 103){
+      periodicTableLocation = document.getElementById("table_1_7_3");
+      periodicTableLocation.appendChild(atomBox);     
+    }  
     if(atomicNum >= 89 && atomicNum <= 102 )
     {
       newColumn = String(atomicNum - 88);
@@ -154,14 +203,16 @@ async function createTables(theAtoms) {
       modal.style.display = 'block';
     };
     
-  }
-
+  } 
+} 
+  console.log("Lord Sesshomaru");
 createTables(atoms);
 
 async function addAtoms(theAtoms) {
   theAtoms = await theAtoms;
   //console.log('Before looping',theAtoms);
   //console.log('Get individual element', theAtoms[0]);
+  //for (let i = 0; i < theAtoms.length; i++) {
   for (let i = 0; i < theAtoms.length; i++) {
     const currentAtom = theAtoms[i];
     const atomBox = document.createElement('div');
